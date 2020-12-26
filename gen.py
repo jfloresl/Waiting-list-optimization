852bdcdbb726a9f8dc56eada34ff148d53521271
jfloresl <jorge.floresl@mail.udp.cl>
2020-12-26 19:08:58 -0300

    nombre personas y cirugias

--- gen.py
+++ gen.py
@@ -7,23 +7,17 @@ dias=['lunes','martes','miercoles','jueves','viernes']
 rut = random.randint(4000000, 24000000)
 dv= random.randint(0,9)
 rut_completo=str(rut)+"-"+str(dv)
-datos = pd.DataFrame({'pabellon': [11, 12, 13, 14],'duracion': rut_completo,'rut': [31, 32, 33, 34]})
 
-'''nombre
-ap_paterno
-ap_materno
-intervencion
-cirujano1
-cirujano2
-paramedica1
-paramedica2
-'''
+datos = pd.DataFrame({'pabellon': rut,'duracion': rut_completo,'rut': rut,'nombre':rut,'ap paterno':rut,'ap materno':rut,'intervencion':c,'cirujano-1':rut,'cirujano-2':rut,'paramedica-1':rut,'paramedica-2':rut})
+
+
 print(rut,"-",dv)
 print(len(n[:327]))
 print(len(n[328:654]))
 print(len(n[655:981]))
 print(len(c))
-writer = pd.ExcelWriter('pandas_multiple.xlsx', engine='xlsxwriter')
+
+writer = pd.ExcelWriter('Libro2.xlsx', engine='xlsxwriter')
 
 datos.to_excel(writer, sheet_name='lunes', index=0)
 
